6e9025bd571a6d83fe9147c91eeb6b5712fa243b
Antonio Vivace <antonio.vivace@cern.ch>
2021-02-04 14:58:22 +0100

    Update dependencies, disable some rules

--- rules/index.js
+++ rules/index.js
@@ -1,14 +1,14 @@
 'use strict';
 
 module.exports = [
-	require('./heading'),
+	//require('./heading'),
 	//require('./badge'),
 	require('./contributing'),
 	require('./git-repo-age'),
 	require('./github'),
 	/// require('./license'),
-	require('./list-item'),
+	//require('./list-item'),
 	require('./no-ci-badge'),
 	require('./spell-check'),
-	require('./toc')
+	//require('./toc')
 ];
